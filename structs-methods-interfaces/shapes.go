f69993104a13c04f93fb8d7916e943022a00ae2a
Travis Kopp <teakopp@github.com>
2021-06-17 16:19:43 -0700

    added triangle shape and refactored

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -2,6 +2,15 @@ package main
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -17,6 +26,10 @@ type Circle struct {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
 
 func Perimeter(rectangle Rectangle) float64 {
